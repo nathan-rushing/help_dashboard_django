69773bfcc9102bd3dc8a84cfe3f6b3a9613344bd
nathan-rushing <158141717+nathan-rushing@users.noreply.github.com>
2025-06-17 13:45:51 +0800

    lagyan ng css

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'online_help',  # Custom app for online help
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
