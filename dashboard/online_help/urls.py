72842c2674a694d7dd3fa2a06f53d248a81168af
Jonathan Bunquin <158141717+nathan-rushing@users.noreply.github.com>
2025-06-23 15:54:32 +0800

    section home ave good

--- dashboard/online_help/urls.py
+++ dashboard/online_help/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('erd/', views.erd, name='erd'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('per_subsection/', views.per_subsection, name='per_subsection'),
+    # path('per_subsection/<str:section_name>/', views.per_subsection, name='per_subsection'),
 ]
