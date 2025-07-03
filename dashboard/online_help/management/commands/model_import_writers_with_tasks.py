390096545d036166930a7ec66366ee9a79d79903
Jonathan Bunquin <158141717+nathan-rushing@users.noreply.github.com>
2025-07-03 15:57:09 +0800

    populate db (without major docu)

---
+++ dashboard/online_help/management/commands/model_import_writers_with_tasks.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from online_help.models import Task, Writers, TaskWriter
+import pandas as pd
+
+class Command(BaseCommand):
+    help = 'Import unique writers from a CSV or Excel file'
+    def handle(self, *args, **kwargs):
+        df = pd.read_excel('online_help/management/dataset/Radiant_2025.1_help_assignments_v3_cleaned.xlsx', sheet_name='2025.1')
+
+        for _, row in df.iterrows():
+            task = Task.objects.create(
+                document=row['Documentation'],
+                section=row['Section'],
+                sub_section=row['Sub-sections'],
+                comments=row.get('Comments', ''),
+                SME=row.get('Subject Matter Expert/Engineering', ''),
+                color=row.get('color', '')
+            )
+
+            writers = str(row.get('Writer', '')).split('\n')
+            for writer_name in writers:
+                writer_name = writer_name.strip()
+                if writer_name:
+                    writer, _ = Writers.objects.get_or_create(writer_name=writer_name)
+                    TaskWriter.objects.get_or_create(task=task, writer=writer)
+
+        self.stdout.write(self.style.SUCCESS('Writers with tasks imported successfully!'))
+
